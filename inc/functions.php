b6a323c654051b83bc1f1ecee34dea5234fa70df
devwael <dev.ahmedwael@gmail.com>
2025-07-30 22:52:53 +0300

    Update `merchant_admin_upgrade_link` function signature

--- inc/functions.php
+++ inc/functions.php
@@ -266,23 +266,36 @@ if ( ! function_exists( 'merchant_admin_upgrade_link' ) ) {
 	/**
 	 * Get the Merchant Pro upgrade link.
 	 *
-	 * @param string $medium  The medium type (link, button, etc.).
-	 * @param string $content The content to display.
+	 * @param string $url  The base URL for the upgrade link.
+	 * @param array  $args The arguments for the upgrade link. (Key-value pairs)
+	 * @param string $type The type of upgrade link.
 	 *
 	 * @return string The upgrade link.
 	 */
-	function merchant_admin_upgrade_link( $medium = 'link', $content = '' ) {
-		$url = 'https://athemes.com/merchant/#pricing';
+	function merchant_admin_upgrade_link( $url, $args = array(), $type = 'plugins-page-upgrade-link' ) {
+		/**
+		 * Filter the upgrade link URL args.
+		 *
+		 * @param array  $args The arguments for the upgrade link.
+		 * @param string $type The type of upgrade link.
+		 *
+		 * @since 2.1.5
+		 */
+		$args = apply_filters( 'merchant_admin_upgrade_link_args', $args, $type );
+		if ( ! empty( $args ) ) {
+			$url = add_query_arg( $args, $url );
+		}
 
 		/**
 		 * Modify upgrade link.
 		 *
 		 * @param string $upgrade Upgrade links.
+		 * @param string $type    The type of upgrade link.
 		 *
-		 * @since 2.1.2
+		 * @since 2.1.5
 		 *
 		 */
-		return apply_filters( 'merchant_upgrade_link', $url );
+		return apply_filters( 'merchant_upgrade_link', $url, $type );
 	}
 }
 
